b811ef333d11ec9ef3598f941bbdb45a3e6c2c3f
Gustavo Henrique de Lima Oliveira <gustavolima@Gustavos-MacBook-Pro.local>
2019-06-11 00:12:23 -0300

    User by id

--- src/main/java/br/com/gustavolima/workshopmongo/service/UserService.java
+++ src/main/java/br/com/gustavolima/workshopmongo/service/UserService.java
@@ -1,12 +1,14 @@
 package br.com.gustavolima.workshopmongo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import br.com.gustavolima.workshopmongo.domain.User;
 import br.com.gustavolima.workshopmongo.repository.UserRepository;
+import br.com.gustavolima.workshopmongo.service.exception.ObjectNotFoundException;
 
 @Repository
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User> fildAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
 }
