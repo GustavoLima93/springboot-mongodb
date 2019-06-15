289180a2482842295dae794a170cb56ead524c99
Gustavo Henrique de Lima Oliveira <gustavolima@Gustavos-MacBook-Pro.local>
2019-06-15 15:24:14 -0300

    User com Delet

--- src/main/java/br/com/gustavolima/workshopmongo/resources/UserResource.java
+++ src/main/java/br/com/gustavolima/workshopmongo/resources/UserResource.java
@@ -48,6 +48,12 @@ public class UserResource {
 		return ResponseEntity.created(uri).build();
 	}
 	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
 //	@RequestMapping(method = RequestMethod.POST)
 //	public ResponseEntity<UserDTO> insert(@RequestBody UserDTO objDTO) {
 //		User obj = service.fromDTO(objDTO);
